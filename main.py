ceaa26ccd64f124ba6f41f85e1fcf2762ef2e31c
willmorrison661@gmail.com <willmorrison661@gmail.com>
2022-07-10 15:19:10 +0100

    lint main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from args import args
-from datetime import datetime, timedelta
+from datetime import datetime as dt, timedelta
 from time import sleep
 import xarray as xr
 import pixpy
@@ -7,69 +7,58 @@ import numpy as np
 import xml.etree.ElementTree as ET
 from os import path
 
-shutter_delay = 0.3 # assumed
+shutter_delay = 0.3  # assumed
 skip_frames_after_shutter_delay = 2
 
+
 def get_config_vars(config_file):
-    
     tree = ET.parse(config_file)
     fps_config = int(float(tree.getroot().find('framerate').text))
     sn_config = int(float(tree.getroot().find('serial').text))
-    
     return {
-        'fps': fps_config, 
+        'fps': fps_config,
         'sn': sn_config,
         }
 
+
 def app_setup():
-    
-    sschedule = pixpy.SnapshotSchedule(
+    ssched = pixpy.SnapshotSchedule(
         file_interval=timedelta(seconds=args.file_interval),
         sample_interval=timedelta(seconds=args.sample_interval),
         sample_repetition=timedelta(seconds=args.sample_repetition),
         )
     shutter = pixpy.Shutter()
-    
     config_vars = get_config_vars(args.config_file)
-    
     pixpy.usb_init_retry(args.config_file)
-    
     sn = pixpy.get_serial()
-    
     if sn == 0:
         raise ValueError("Invalid serial number")
-        
     if config_vars['sn'] != sn:
-        raise ValueError(
-            f'sn of attached camera ({sn}) is different to sn in {args.config_file}'
-            )
-    
+        raise ValueError(f'Found camera {sn} but expceted {args.config_file}')
     pixpy.set_shutter_mode(0)
     shutter.trigger()
     sleep(shutter_delay * 2)
-    
-    return sschedule, config_vars, shutter
+    return ssched, config_vars, shutter
 
-def get_file_name(sschedule, sn):
-    
-    file_end_raw = sschedule.current_file_time_end()
+
+def get_file_name(ssched, sn):
+    file_end_raw = ssched.current_file_time_end()
     return str(sn) + '_' + \
-        str(file_end_raw).replace("-", "").replace(":", "").replace(" ", "") 
+        str(file_end_raw).replace("-", "").replace(":", "").replace(" ", "")
 
-def pixpy_app(sschedule, config_vars, shutter):
-    
-    print(sschedule)
-    
+
+def pixpy_app(ssched, config_vars, shutter):
+    print(ssched)
     width, height = pixpy.get_thermal_image_size()
-    sample_timesteps_remaining = sschedule.sample_timesteps_remaining()
-    print(datetime.utcnow())
+    sample_timesteps_remaining = ssched.sample_timesteps_remaining()
+    print(dt.utcnow())
     print(sample_timesteps_remaining)
     print(f'shutter_delay {shutter_delay}')
-    sample_interval_s = sschedule.sample_interval.total_seconds()
+    sample_interval_s = ssched.sample_interval.total_seconds()
     print(f'sample_interval_s {sample_interval_s}')
-    file_name = get_file_name(sschedule, config_vars['sn'])
-    
+    file_name = get_file_name(ssched, config_vars['sn'])
     time_timeseries = np.empty(sample_timesteps_remaining)
+    # todo: change to just one 4D array with x, y, time, variable
     image_snapshot_timeseries = np.empty((sample_timesteps_remaining, height, width), dtype=np.uint16)
     image_median_timeseries = np.empty((sample_timesteps_remaining, height, width), dtype=np.uint16)
     image_min_timeseries = np.empty((sample_timesteps_remaining, height, width), dtype=np.uint16)
@@ -85,28 +74,30 @@ def pixpy_app(sschedule, config_vars, shutter):
     n_images = int((sample_interval_s * config_vars['fps']) + 0.5)
     print(f'n_images {n_images}')
     print(f"fps {config_vars['fps']}")
-    
-    time_until_next_interval = sschedule.current_sample_start() - \
-        datetime.utcnow() - timedelta(seconds=shutter_delay)
+    time_until_next_interval = ssched.current_sample_start() - \
+        dt.utcnow() - timedelta(seconds=shutter_delay)
     print(f'time_until_next_interval {time_until_next_interval}')
     while (time_until_next_interval.total_seconds() - shutter_delay) < 0:
-        time_until_next_interval = sschedule.current_sample_start() - \
-            datetime.utcnow() - timedelta(seconds=shutter_delay)
+        time_until_next_interval = ssched.current_sample_start() - \
+            dt.utcnow() - timedelta(seconds=shutter_delay)
     print(f"sleeping for {time_until_next_interval.total_seconds()}")
     sleep(time_until_next_interval.total_seconds())
     for j in range(0, sample_timesteps_remaining):
-        print(f'started n_interval_timestep {j + 1} / {sample_timesteps_remaining} at {datetime.utcnow()}')        
+        print(
+                f'started n_interval_timestep {j + 1} / '
+                f'{sample_timesteps_remaining} at {dt.utcnow()}'
+              )
         shutter.trigger()
         print(f'shutter triggered {shutter.triggers} times')
         sleep(shutter_delay)
-        print(f'waited for shutter until {datetime.utcnow()}')        
-        interval_start_time = datetime.utcnow()
+        print(f'waited for shutter until {dt.utcnow()}')
+        interval_start_time = dt.utcnow()
         images_raw = np.empty((n_images, height, width), dtype=np.uint16)
         for i in range(0, n_images):
             image, meta = pixpy.get_thermal_image_metadata(width, height)
             images_raw[i, :, :] = image
-            #print(f'got image {i} / {n_images} at {datetime.utcnow()}')
-        interval_end_time = datetime.utcnow()
+            # print(f'got image {i} / {n_images} at {dt.utcnow()}')
+        interval_end_time = dt.utcnow()
         print(f'interval has timestamp {interval_end_time}')
         dtime = interval_end_time - interval_start_time
         fps = n_images / dtime.total_seconds()
@@ -124,22 +115,21 @@ def pixpy_app(sschedule, config_vars, shutter):
         fps_timeseries[j] = fps
         nsamples_timeseries[j] = n_images
         if j != (sample_timesteps_remaining - 1):
-            next_interval_time = interval_start_time + sschedule.sample_repetition
-            current_time = datetime.utcnow()
+            next_interval_time = interval_start_time + ssched.sample_repetition
+            current_time = dt.utcnow()
             wait_time_until_next_interval_s = (
                 next_interval_time - current_time
                 ).total_seconds() - shutter_delay
-                
-            print(f'Time until next interval: {wait_time_until_next_interval_s}')
+            print(f'Next interval in: {wait_time_until_next_interval_s} s.')
             if wait_time_until_next_interval_s < 0:
-                print("Next interval start time missed. Slow the sampling rate and/or fps.")
+                # todo: send to log file
+                print("Next interval missed. Slow the sampling rate/fps.")
                 wait_time_until_next_interval_s = 0
             sleep(wait_time_until_next_interval_s)
         else:
-            next_sample_start_check = sschedule.current_sample_start()
+            next_sample_start_check = ssched.current_sample_start()
             x = np.arange(0, 160)
             y = np.flip(np.arange(0, 120))
-            
             ds = xr.Dataset(
                 data_vars=dict(
                     t_b_median=(["time", "y", "x"], image_median_timeseries),
@@ -157,15 +147,14 @@ def pixpy_app(sschedule, config_vars, shutter):
                 ),
                 coords=dict(
                     x=x,
-                    y=y,        
-                    time=[datetime.utcfromtimestamp(i) for i in time_timeseries],
+                    y=y,
+                    time=[dt.utcfromtimestamp(i) for i in time_timeseries],
                 ),
-                attrs=dict(description="pixpy",
-                            serial=config_vars['sn']),
+                attrs=dict(description="pixpy", serial=config_vars['sn']),
             )
-            ds.to_netcdf(path.join(args.output_directory, f'{file_name}.nc'), 
-                          encoding={
-                              'time': {'dtype': 'i4'}, 
+            ds.to_netcdf(path.join(args.output_directory, f'{file_name}.nc'),
+                         encoding={
+                              'time': {'dtype': 'i4'},
                               't_b_median': {'zlib': True, "complevel": 5},
                               't_b_min': {'zlib': True, "complevel": 5},
                               't_b_max': {'zlib': True, "complevel": 5},
@@ -173,12 +162,12 @@ def pixpy_app(sschedule, config_vars, shutter):
                               't_b_snapshot': {'zlib': True, "complevel": 5},
                               'nsamples': {'zlib': True, "complevel": 5},
                               })
-            if (next_sample_start_check < datetime.utcnow()):
+            if (next_sample_start_check < dt.utcnow()):
                 print("missed first sample due to writing data to disk")
 
 
 # todo - wrap in try catch and redo usb_init as appropriate
 if __name__ == "__main__":
-    sschedule, config_vars, shutter = app_setup()
+    ssched, config_vars, shutter = app_setup()
     while True:
-        pixpy_app(sschedule, config_vars, shutter)
+        pixpy_app(ssched, config_vars, shutter)
