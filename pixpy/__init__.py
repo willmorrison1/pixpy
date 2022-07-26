4c44fce8b8f769656d970cf821811a7a3e1ee45f
willmorrison1 <willmorrison661@gmail.com>
2022-07-26 19:00:26 +0200

    Update __init__.py

--- pixpy/__init__.py
+++ pixpy/__init__.py
@@ -1,1 +1,5 @@
-import config, pixpy
+from . import config, args
+from pixpy.pixpy import (
+    Shutter, SnapshotSchedule, usb_init_retry, set_shutter_mode, 
+    get_thermal_image_size, get_serial, get_thermal_image_metadata, terminate,
+    )
